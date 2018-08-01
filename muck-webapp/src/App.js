b4254cb5c6e8d8038a387b77aca526f13ad87ac6
Graham Cox <graham@grahamcox.co.uk>
2018-08-01 18:42:20 +0100

    Added in Semantic UI

--- muck-webapp/src/App.js
+++ muck-webapp/src/App.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+// @flow
 
-class App extends Component {
-  render() {
+import React from 'react';
+
+export default function App() {
     return (
-      <div className="App">
-      </div>
+        <div className="App">
+        </div>
     );
-  }
 }
-
-export default App;
