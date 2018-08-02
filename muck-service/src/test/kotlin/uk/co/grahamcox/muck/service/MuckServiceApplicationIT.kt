736b539eae3234f1905a71b6f552219a6bedd09d
Graham Cox <graham@grahamcox.co.uk>
2018-08-02 13:13:04 +0100

    Set up JUnit 5 and Failsafe

--- muck-service/src/test/kotlin/uk/co/grahamcox/muck/service/MuckServiceApplicationIT.kt
+++ muck-service/src/test/kotlin/uk/co/grahamcox/muck/service/MuckServiceApplicationIT.kt
@@ -3,11 +3,13 @@ package uk.co.grahamcox.muck.service
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
-class MuckServiceApplicationTests {
+@SpringBootTest(classes = [MuckServiceApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class MuckServiceApplicationIT {
 
 	@Test
 	fun contextLoads() {
