232b5f2021fe097df5724c6b5c42c843290da993
Graham Cox <graham@grahamcox.co.uk>
2018-08-06 12:59:22 +0100

    Set up the database connection, including an embedded PostgreSQL is needed

--- muck-service/src/main/kotlin/uk/co/grahamcox/muck/service/MuckServiceApplication.kt
+++ muck-service/src/main/kotlin/uk/co/grahamcox/muck/service/MuckServiceApplication.kt
@@ -2,8 +2,13 @@ package uk.co.grahamcox.muck.service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
+import uk.co.grahamcox.muck.service.database.DatabaseConfig
 
 @SpringBootApplication
+@Import(
+        DatabaseConfig::class
+)
 class MuckServiceApplication
 
 fun main(args: Array<String>) {
