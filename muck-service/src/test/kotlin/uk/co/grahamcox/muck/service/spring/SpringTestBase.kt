5ad88e64251f3ffc48be9aabd76af96294475587
Graham Cox <graham@grahamcox.co.uk>
2018-08-20 18:35:29 +0100

    Rudimentary tests for the transaction manager

--- muck-service/src/test/kotlin/uk/co/grahamcox/muck/service/spring/SpringTestBase.kt
+++ muck-service/src/test/kotlin/uk/co/grahamcox/muck/service/spring/SpringTestBase.kt
@@ -2,10 +2,13 @@ package uk.co.grahamcox.muck.service.spring
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
+import org.neo4j.driver.v1.Driver
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import uk.co.grahamcox.muck.service.MuckServiceApplication
+import uk.co.grahamcox.muck.service.acceptance.database.DatabaseCleaner
 
 /**
  * Base class for Spring based tests
@@ -14,12 +17,16 @@ import uk.co.grahamcox.muck.service.MuckServiceApplication
 @SpringBootTest(classes = [MuckServiceApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class SpringTestBase {
+    /** The database cleaner */
+    @Autowired
+    private lateinit var databaseCleaner: DatabaseCleaner
 
     /**
      * Clear the database before each test
      */
     @BeforeEach
     fun clearDatabase() {
+        databaseCleaner.clean()
     }
 
     /**
