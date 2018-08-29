5c993d4b26e6cd01d4a8c5766c34180b2eee8e2c
Graham Cox <graham@grahamcox.co.uk>
2018-08-29 16:14:56 +0100

    Implement HAL to return the list of supported External Authentication Services

--- muck-service/src/main/kotlin/uk/co/grahamcox/muck/service/authentication/external/rest/ExternalAuthenticationController.kt
+++ muck-service/src/main/kotlin/uk/co/grahamcox/muck/service/authentication/external/rest/ExternalAuthenticationController.kt
@@ -1,9 +1,14 @@
 package uk.co.grahamcox.muck.service.authentication.external.rest
 
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 import uk.co.grahamcox.muck.service.authentication.external.AuthenticationService
+import uk.co.grahamcox.muck.service.rest.hal.Link
+import java.net.URI
 
 /**
  * Controller for managing authentication by a third-party provider
@@ -18,7 +23,27 @@ class ExternalAuthenticationController(
      * @return the list of supported providers
      */
     @RequestMapping(method = [RequestMethod.GET])
-    fun getProviders(): List<String> {
-        return authenticationServices.map { it.id }
+    fun getProviders(): ResponseEntity<ExternalAuthenticationServicesModel> {
+        val result = ExternalAuthenticationServicesModel(
+                links = ExternalAthenticationServicesLinks(
+                        self = Link(
+                                href = URI("/api/authentication/external")
+                        ),
+                        services = authenticationServices
+                                .map { it.id }
+                                .sorted()
+                                .map {
+                                    Link(
+                                            href = URI("/api/authentication/external/$it"),
+                                            name = it,
+                                            type = null
+                                    )
+                                }
+                )
+        )
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf("application/hal+json"))
+                .body(result)
     }
 }
---
+++ muck-service/src/main/kotlin/uk/co/grahamcox/muck/service/rest/hal/Link.kt
@@ -0,0 +1,17 @@
+package uk.co.grahamcox.muck.service.rest.hal
+
+import java.net.URI
+
+/**
+ * Representation of a single link in a HAL document
+ */
+data class Link(
+        val href: URI,
+        val templated: Boolean? = false,
+        val type: String? = "application/hal+json",
+        val deprecation: String? = null,
+        val name: String? = null,
+        val profile: URI? = null,
+        val title: String? = null,
+        val hreflang: String? = null
+)
