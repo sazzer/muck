fd3647d5438e1794543659e2e5fcb5dd0ed1ff64
Graham Cox <graham@grahamcox.co.uk>
2018-09-08 10:11:45 -0400

    A couple of minor tweaks to the Social Login buttons

--- muck-webapp/src/ui/landing/login/SocialLogins.js
+++ muck-webapp/src/ui/landing/login/SocialLogins.js
@@ -4,6 +4,11 @@ import React from 'react';
 import {Button, Icon} from "semantic-ui-react";
 import {Interpolate} from "react-i18next";
 
+/** The colours to use for the buttons */
+const BUTTON_COLOURS = {
+    'google': 'google plus'
+};
+
 type SocialLoginsProps = {
     buttons: Array<string>
 }
@@ -14,7 +19,7 @@ type SocialLoginsProps = {
  */
 export default function SocialLogins({buttons}: SocialLoginsProps) {
     const buttonMarkup = buttons.map(button => (
-        <Button color={button} fluid>
+        <Button color={BUTTON_COLOURS[button]} fluid key={button}>
             <Icon name={button} />
             <Interpolate i18nKey={`page.landing.login.socialLogins.${button}`} />
         </Button>
