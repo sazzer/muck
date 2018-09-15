fd0a352120db126f516a6285d6b549a5d9c0cb6d
Graham Cox <graham@grahamcox.co.uk>
2018-09-15 03:09:01 -0400

    Support for storing user profiles into the state

--- muck-webapp/src/users/userProfiles.js
+++ muck-webapp/src/users/userProfiles.js
@@ -1,11 +1,26 @@
 // @flow
 
 /** The type to represent a user */
-import {call} from 'redux-saga/effects';
+import {call, put} from 'redux-saga/effects';
 import {getApiRoot, loadResource} from "../api";
 
-export type UserProfile = {
+/**
+ * Type representing the details of a login with a provider
+ */
+export type UserLogin = {
+    provider: string,
+    providerId: string,
+    displayName: string
+}
 
+/**
+ * Type representing a user
+ */
+export type UserProfile = {
+    userLink: string,
+    email?: string,
+    displayName: string,
+    logins: Array<UserLogin>
 };
 
 /** The shape of the state for this sub-module */
@@ -21,9 +36,19 @@ export type LoadUserProfileAction = {
     }
 };
 
+/** The shape of the action for storing a specific user into the store */
+export type StoreUserProfileAction = {
+    type: string,
+    payload: {
+        user: UserProfile
+    }
+}
 
 /** Action key for loading a user from the server */
 const LOAD_USER_PROFILE_ACTION = "USERS/LOAD_USER_PROFILE_ACTION";
+
+/** Action key for storing a user into the store */
+const STORE_USER_PROFILE_ACTION = "USERS/STORE_USER_PROFILE_ACTION";
 
 /**
  * Construct the action for loading a user from the server
@@ -34,6 +59,19 @@ export function loadUserProfile(userId: string) : LoadUserProfileAction {
         type: LOAD_USER_PROFILE_ACTION,
         payload: {
             userId
+        }
+    };
+}
+
+/**
+ * Construct the action for storing a user into the store
+ * @param user the user to store
+ */
+export function storeUserProfile(user: UserProfile) : StoreUserProfileAction {
+    return {
+        type: STORE_USER_PROFILE_ACTION,
+        payload: {
+            user
         }
     };
 }
@@ -50,8 +88,24 @@ export function* loadUserProfileSaga(action: LoadUserProfileAction): Generator<a
                 id: action.payload.userId
             }
         });
-        console.log(user);
+
+        const userProfile = {
+            userLink: user.getLink('self').href,
+            email: user.data.email,
+            displayName: user.data.displayName,
+            logins: user.data.logins
+        };
+        yield put(storeUserProfile(userProfile));
     }
+}
+
+/**
+ * Mutation for storing the user profile into the state
+ * @param state the state to update
+ * @param action the action to update from
+ */
+export function storeUserProfileMutation(state: UserProfilesState, action: StoreUserProfileAction) {
+    state.users[action.payload.user.userLink] = action.payload.user;
 }
 
 /** The representation of this sub-module */
@@ -62,6 +116,7 @@ export const module = {
     actions: {
     },
     mutations: {
+        [STORE_USER_PROFILE_ACTION]: storeUserProfileMutation
     },
     sagas: {
         [LOAD_USER_PROFILE_ACTION]: loadUserProfileSaga
