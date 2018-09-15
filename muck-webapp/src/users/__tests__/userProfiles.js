c7847b0358a24cb5641447def2ac5c5c87c2626a
Graham Cox <graham@grahamcox.co.uk>
2018-09-15 06:31:56 -0400

    User Menu displayed when there is a current user

---
+++ muck-webapp/src/users/__tests__/userProfiles.js
@@ -0,0 +1,76 @@
+// @flow
+
+import * as testSubject from '../userProfiles';
+import type {StoreUserProfileAction} from "../userProfiles";
+
+const user = {
+    userLink: 'http://example.com',
+    displayName: 'Test User',
+    logins: []
+};
+
+describe('loadUserProfile', () => {
+    it('Produces the correct action', () => {
+        expect(testSubject.loadUserProfile('someUserId')).toEqual({
+            type: 'USERS/LOAD_USER_PROFILE_ACTION',
+            payload: {
+                userId: 'someUserId'
+            }
+        })
+    })
+});
+
+describe('storeUserProfile', () => {
+    it('Produces the correct action', () => {
+        expect(testSubject.storeUserProfile(user)).toEqual({
+            type: 'USERS/STORE_USER_PROFILE_ACTION',
+            payload: {
+                user: user
+            }
+        })
+    })
+});
+
+describe('storeUserProfileMutation', () => {
+    it('Correctly updates the state', () => {
+        const state = {
+            ...testSubject.module.initialState
+        };
+        const action: StoreUserProfileAction = {
+            type: 'USERS/STORE_USER_PROFILE_ACTION',
+            payload: {
+                user: user
+            }
+        };
+        testSubject.storeUserProfileMutation(state, action);
+
+        expect(state).toEqual({
+            users: {
+                'http://example.com': user
+            }
+        });
+    });
+});
+
+describe('selectUserById', () => {
+    describe('From an empty state', () => {
+        const state = {
+            ...testSubject.module.initialState
+        };
+
+        it('Returns nothing', () => {
+            expect(testSubject.selectUserById(state, 'userId')).toBeUndefined();
+        });
+    });
+    describe('From a populated state', () => {
+        const state = {
+            users: {
+                userId: user
+            }
+        };
+
+        it('Returns the correct value', () => {
+            expect(testSubject.selectUserById(state, 'userId')).toBe(user);
+        });
+    });
+});
