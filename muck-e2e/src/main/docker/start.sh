e85e6f4a54c73758e7ea03b6c6b37b6d8745dc1b
Graham Cox <graham@grahamcox.co.uk>
2018-09-27 08:56:16 +0100

    Binned Groovy, Spock and Geb

--- muck-e2e/src/main/docker/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x
-set -e
-
-for BROWSER in ${BROWSERS}; do
-    java -Dgeb.env=remote -DbaseUrl=http://${WWW_HOST} -Dselenium.url=http://${SELENIUM_HOST}/wd/hub -DbrowserName=${BROWSER} -jar e2e.jar
-done
--- muck-e2e/src/main/groovy/uk/co/grahamcox/muck/e2e/E2eSpecBase.groovy
+++
@@ -1,9 +0,0 @@
-package uk.co.grahamcox.muck.e2e
-
-import geb.spock.GebReportingSpec
-
-/**
- * Base class for all End-to-end tests
- */
-class E2eSpecBase extends GebReportingSpec {
-}
